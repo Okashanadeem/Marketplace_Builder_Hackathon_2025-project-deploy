54b05530a6d749602581dfec6cfa1378cb31fc85
Okashanadeem <okashanadeem0101@gmail.com>
2025-01-22 10:18:30 +0500

    Solve error try 1

--- my-app/sanity/schemaTypes/index.ts
+++ my-app/sanity/schemaTypes/index.ts
@@ -2,8 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import product from '../schema/product'
 import order from '../schema/order'
 import deliveryZone from '../schema/deliveryZone'
-import user from '../schema/user'
+import customer from '../schema/customer'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, user, order, deliveryZone],
+  types: [product, customer, order, deliveryZone],
 }
